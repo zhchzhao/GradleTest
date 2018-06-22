25f3f3a9d39c0f608f94d196ee933be14457ad8c
zhchzhao <zhchzhao@cn.ibm.com>
2018-06-22 23:59:14 +0800

    first commit

---
+++ DevOpsTest/src/main/java/GradleTest.java
@@ -0,0 +1,59 @@
+package main.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class GradleTest extends HttpServlet{
+	
+	
+
+
+
+
+
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GradleTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**Story 1700864
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//((HttpServletRequest) request).getRequestDispatcher("/software/ram/akamai/sureroute-test-object.html").forward(((HttpServletRequest) request), ((HttpServletResponse) response));
+		
+		
+		response.setHeader("content-type", "text/html;charset=ISO-8859-1");
+		response.setContentType("text/html;charset=ISO-8859-1");
+		PrintWriter out = response.getWriter();
+		out.println("*****************test servlet by Gradle build******************");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("*****************:doPost");
+		doGet(request, response);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("111111111111111111111111111111111111111");
+	}
+}
