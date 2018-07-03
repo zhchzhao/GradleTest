3a34a4e8756f106b718a20f61019f7664812db4a
zhchzhao <zhchzhao@cn.ibm.com>
2018-07-04 00:16:19 +0800

    for jsp value

---
+++ DevOpsTest/WebContent/index.jsp
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>test page title</title>
+</head>
+
+<body>
+	<form action="test.do" method="get">
+		<input type="text" id="userName" name="userName" value="" />
+		<p />
+		<input type="text" id="passWord" name="passWord" value="" />
+		<p />
+		<input type="submit" value="submit" />
+	</form>
+
+</body>
+
+
+
+</html>
--- DevOpsTest/src/main/java/GradleTest.java
+++ DevOpsTest/src/main/java/GradleTest.java
@@ -8,46 +8,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-public class GradleTest extends HttpServlet{
-	
-	
-
-
-
-
+public class GradleTest extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GradleTest() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**Story 1700864
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	/**
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public GradleTest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Story 1700864
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		//((HttpServletRequest) request).getRequestDispatcher("/software/ram/akamai/sureroute-test-object.html").forward(((HttpServletRequest) request), ((HttpServletResponse) response));
-		
-		
-		response.setHeader("content-type", "text/html;charset=ISO-8859-1");
-		response.setContentType("text/html;charset=ISO-8859-1");
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ((HttpServletRequest)
+		// request).getRequestDispatcher("/software/ram/akamai/sureroute-test-object.html").forward(((HttpServletRequest)
+		// request), ((HttpServletResponse) response));
+
+		response.setHeader("content-type", "text/html;charset=UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("*****************test servlet by Gradle build******************");
+		out.println("**********  "+request.getParameter("userName")+"    "+request.getParameter("passWord")+"  ***********");
 		out.flush();
 		out.close();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("*****************:doPost");
 		doGet(request, response);
